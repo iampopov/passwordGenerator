e96495d8d0e220e11b1fa9cc00cc21f12089a1e0
IamPopov <ivantsar@gmail.com>
2019-12-12 18:41:24 -0700

    created random password generator code

--- script.js
+++ script.js
@@ -13,10 +13,17 @@ var lowerCase = ["a", "b", "c",	"d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 //Uppercase characters
 
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var pool = specialCharacters.concat(numericCharacters, lowerCase, upperCase); // works well
+var randPwd = [];
+
+for (var i = 0; i < 8; i++) {
+    randPwd.push(pool[Math.floor(Math.random() * pool.length)]);
+}
+
+console.log(randPwd.join(''));
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
 
 // Write password to the #password input
 function writePassword() {
@@ -28,6 +35,7 @@ function writePassword() {
   copyBtn.removeAttribute("disabled");
   copyBtn.focus();
 }
+
 
 function copyToClipboard() {
   // BONUS 
